955e7ad132947f94d4ee286dbc9f53640f526c36
Avi <avi.leeker@gmail.com>
2025-11-06 22:41:36 +0200

    more vercel/vite env troubleshooting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
-// https://vite.dev/config/
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load .env locally
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': import.meta.env.VITE_API_URL,
+      '/api': process.env.VITE_API_URL || 'http://localhost:3000/api',
     },
   },
   resolve: {
